4a4207bd18e0a6b898cf869d6c187f0e6b8c4b66
rigma <rigbuntu@gmail.com>
2019-07-13 19:42:16 +0200

    fix: wrong return type and add enum for different values

--- src/generator.rs
+++ src/generator.rs
@@ -1,7 +1,13 @@
 pub trait Generator {
     type Error;
 
-    fn get_value(&self) -> Result<&str, Self::Error>;
+    fn get_value(&mut self) -> Result<&str, Self::Error>;
 
     fn get_otp_auth_uri(&self) -> Result<&str, Self::Error>;
+}
+
+pub enum GeneratorAlgorithm {
+    HmacSha1,
+    HmacSha256,
+    HmacSha512,
 }

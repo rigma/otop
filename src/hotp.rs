4ee6333c26bfed8b0a33ecc091fdf88dbec06c38
rigma <rigbuntu@gmail.com>
2019-07-14 21:43:09 +0200

    chore: add license and readme files

--- src/hotp.rs
+++ src/hotp.rs
@@ -1,3 +1,11 @@
+// Copyright 2019 Romain Failla
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 //! Provides an HOTP value generator.
 //!
 //! This generator can be used to generate HOTP value to perform a 2FA authentication

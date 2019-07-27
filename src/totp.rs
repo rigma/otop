c9ddd287a08c9acd3f7764a9df3e51c551405e0f
rigma <rigbuntu@gmail.com>
2019-07-27 16:33:00 +0200

    feat: add generator implementation

--- src/totp.rs
+++ src/totp.rs
@@ -8,7 +8,6 @@
 
 use crate::crypto;
 use crate::generator::*;
-use std::default::Default;
 use std::error::Error;
 use std::fmt;
 use std::time::*;
@@ -52,7 +51,7 @@ pub struct TotpGenerator {
     pub algorithm: GeneratorAlgorithm,
     digits: u8,
     epoch: SystemTime,
-    time_step: u64
+    period: u64
 }
 
 impl TotpGenerator {
@@ -65,7 +64,7 @@ impl TotpGenerator {
             algorithm: DEFAULT_ALGORITHM,
             digits: DEFAULT_DIGITS,
             epoch: DEFAULT_EPOCH,
-            time_step: DEFAULT_TIME_STEP,
+            period: DEFAULT_TIME_STEP,
         }
     }
 
@@ -93,18 +92,98 @@ impl TotpGenerator {
         &self.epoch
     }
 
-    pub fn set_time_step(&mut self, step: u64) -> Result<(), TotpError> {
+    pub fn set_period(&mut self, step: u64) -> Result<(), TotpError> {
         if step == 0 {
             return Err(TotpError {
                 kind: String::from("TOTP time step cannot be null!"),
             });
         }
 
-        self.time_step = step;
+        self.period = step;
         Ok(())
     }
 
-    pub fn get_time_step(&self) -> &u64 {
-        &self.time_step
+    pub fn get_period(&self) -> &u64 {
+        &self.period
+    }
+
+    pub fn get_time_counter(&self) -> Result<u64, SystemTimeError> {
+        let duration = SystemTime::now().duration_since(self.epoch)?;
+
+        Ok((duration.as_secs() as f64 / self.period as f64).floor() as u64)
+    }
+
+    fn generate_value(&self, message: &[u8]) -> Result<Vec<u8>, crypto::CryptoError> {
+        match self.algorithm {
+            GeneratorAlgorithm::HmacSha1 => crypto::hmac_sha1(&self.secret, message),
+            GeneratorAlgorithm::HmacSha256 => crypto::hmac_sha256(&self.secret, message),
+            GeneratorAlgorithm::HmacSha512 => crypto::hmac_sha512(&self.secret, message),
+        }
+    }
+}
+
+impl Generator for TotpGenerator {
+    type Error = TotpError;
+
+    fn get_value(&mut self) -> Result<String, Self::Error> {
+        // Generates the current time counter
+        let time_counter = self.get_time_counter();
+        if let Err(_) = time_counter {
+            return Err(Self::Error {
+                kind: String::from("Current time counter cannot be generated!"),
+            });
+        }
+
+        // Parsing time counter value into a HMAC-SHA message
+        let message = encode_counter(&time_counter.unwrap());
+
+        // Generates HMAC-SHA hashe
+        let hmac = self.generate_value(&message);
+        if let Err(_) = hmac {
+            return Err(Self::Error {
+                kind: String::from("HMAC-SHA cipher error"),
+            });
+        }
+
+        // Applying the RFC 4226 offset
+        let value = otp_offset(&hmac.unwrap());
+
+        // Serializing value into a string representation
+        Ok(otp_value_to_string(&value, &self.digits))
+    }
+
+    fn get_otp_auth_uri(&self) -> Result<String, Self::Error> {
+        use data_encoding::BASE32;
+
+        let mut uri = Url::parse("otpauth://totp/").unwrap();
+
+        // Setting label part (Provider and account)
+        match uri.path_segments_mut() {
+            Ok(mut path) => path.push(&format!("{}:{}", self.provider, self.account)),
+            _ => {
+                return Err(Self::Error {
+                    kind: String::from("Wrong URI base"),
+                });
+            }
+        };
+
+        // Setting URI parameters
+        uri.query_pairs_mut()
+            .append_pair("secret", &BASE32.encode(&self.secret))
+            .append_pair(
+                "algorithm",
+                match self.algorithm {
+                    GeneratorAlgorithm::HmacSha1 => "SHA1",
+                    GeneratorAlgorithm::HmacSha256 => "SHA256",
+                    GeneratorAlgorithm::HmacSha512 => "SHA512",
+                },
+            )
+            .append_pair("period", &self.period.to_string());
+
+        if let Some(issuer) = &self.issuer {
+            uri.query_pairs_mut().append_pair("issuer", &issuer);
+        }
+
+        Ok(String::from(uri.as_str()))
     }
 }
